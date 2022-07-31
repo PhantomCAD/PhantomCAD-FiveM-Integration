41cfb420557ce7f729aa5bffdca3eb913cd93749
JipyTheDev <joshua66553@gmail.com>
2022-07-31 02:15:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const config = {
       // Name of the community.
       name: "Test",
       // Name of the communityCommunity ID.
-      CommunityID: "yyl9xxKfsDHcETK",
+      CommunityID: "Fill-in-your-ID",
       // Server ID.
-      ServerID: "?06?6CYJAHO}>30O:<4@>IBMT",
+      ServerID: "Fill-in-your-ID",
       // The State The Server.
       state: "Los Santos",
       // The 911/999 Number.
@@ -13,7 +13,7 @@ const config = {
     },
   
     // make sure this is the model name and it is not conflickting with anyother models
-    Vehicles: [ "PRIMO", "HellcatRed" ],
+    Vehicles: [ "PRIMO", "Vehicle" ],
     
     alert: {
       // Text to be displayed for Emergency Traffic Only Alert
